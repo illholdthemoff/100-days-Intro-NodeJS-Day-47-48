63f7a5e2c2d43a18a0f2206fb48aeb0a96994982
dowens <dowens@smartapp.com>
2022-02-18 17:27:53 -0500

    sgyt the fuck up

--- js/app.js
+++ js/app.js
@@ -3,17 +3,37 @@
 
 // console.log(userName);
 
+const fs = require("fs");
+
 const express = require("express");
+const path = require("path");
 
 const app = express();
+
+app.use(express.urlencoded({extended: false}));
 
 app.get("/currenttime", function (req, res) {
   res.send("<h1>" + new Date().toISOString() + "</h1>");
 }); // localhost:3000/currenttime
 
 app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>");
+  res.send("<form action='/store-user' method='POST'><label>Your Name</label><input type='text' name='username'><button>Submit</button></form>");
 }); // localhost:3000/
+
+app.post("/store-user", function(req, res) {
+  const userName = req.body.username;
+
+  const filePath = path.join(__dirname, "data", "users.json");
+
+const fileData = fs.readFileSync(filePath); // reads data from the file path
+const existingUsers = JSON.parse(fileData); //converts the data into something useful
+
+existingUsers.push(userName);
+
+  fs.writeFileSync(filePath, JSON.stringify(existingUsers));
+
+  res.send("<h1>Username stored</h1>");
+});
 
 app.listen(3000);
 
